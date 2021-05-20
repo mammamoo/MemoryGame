ce4eb9b9b116408ab35c203d8cae6fca9d18c2b5
Zharkynai Maratova <45614762+mammamoo@users.noreply.github.com>
2021-05-21 03:06:10 +0600

    Add files via upload

---
+++ source/repos/MemoryGame1/MemoryGame1/MemoryGame.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryGame1
+{
+    public partial class MemoryGame : Form
+    {
+        Random random = new Random();
+
+        List<String> icons = new List<String>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k", "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
+        Label firstClicked, secondClicked;
+        public MemoryGame()
+        {
+            InitializeComponent();
+            AssignIconToSquares();
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+            if (firstClicked != null && secondClicked != null)
+                return;
+
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel == null)
+                return;
+
+            if (clickedLabel.ForeColor == Color.Black)
+                return;
+
+            if(firstClicked == null)
+            {
+                firstClicked = clickedLabel;
+                firstClicked.ForeColor = Color.Black;
+                return;
+            }
+
+            secondClicked = clickedLabel;
+            secondClicked.ForeColor = Color.Black;
+
+            CheckForWinner();
+
+            if (firstClicked.Text == secondClicked.Text)
+            {
+                firstClicked = null;
+                secondClicked = null;
+            }
+            else
+                timer1.Start();
+        }
+        private void CheckForWinner()
+        {
+            Label label;
+            for (int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
+            {
+                label = tableLayoutPanel1.Controls[i] as Label;
+
+                if (label != null && label.ForeColor == label.BackColor)
+                    return;
+            }
+            MessageBox.Show("Поздравляем! Вы нашли все совпадения!");
+                Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            firstClicked = null;
+            secondClicked = null;
+        }
+
+        private void MemoryGame_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AssignIconToSquares()
+        {
+            Label label;
+            int randomNumber;
+
+            for (int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
+            {
+                if (tableLayoutPanel1.Controls[i] is Label)
+                    label = (Label)tableLayoutPanel1.Controls[i];
+                else
+                    continue;
+                randomNumber = random.Next(0, icons.Count);
+                label.Text = icons[randomNumber];
+
+                icons.RemoveAt(randomNumber);
+            }
+        }
+
+    }
+}
